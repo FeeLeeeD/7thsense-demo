b570b2cd7612c1d9e8b13187657086ca1dc459d2
FeeLeeeD <stanislav@jinteki.dev>
2024-09-13 16:39:33 +0500

    engagement segments chart

--- src/components/pages/account/tabs/audience-analytics-tab/index.tsx
+++ src/components/pages/account/tabs/audience-analytics-tab/index.tsx
@@ -3,6 +3,7 @@ import { Grid, Stack } from "@chakra-ui/react";
 import { ChartWrapper } from "~components/charts/chart-wrapper";
 import { InitiationRatesChart } from "./charts/initiation-rates";
 import { PredictedEnrollmentRateChart } from "./charts/predicted-enrollment-rate";
+import { EngagementSegmentsChart } from "./charts/engagement-segments";
 
 export const AudienceAnalyticsTab = () => {
   return (
@@ -17,6 +18,10 @@ export const AudienceAnalyticsTab = () => {
       <Grid gridTemplateColumns="1fr 1fr" gridGap="xxlarge">
         <ChartWrapper title="Predicted enrollment rate with recycling">
           <PredictedEnrollmentRateChart />
+        </ChartWrapper>
+     
+        <ChartWrapper title="Engagements segments chart">
+          <EngagementSegmentsChart />
         </ChartWrapper>
       </Grid>
     </Stack>
